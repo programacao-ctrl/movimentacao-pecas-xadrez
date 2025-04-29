dedf7e10f7c42a80ef757451439a914e0180498f
programacao-ctrl <werykgithub@gmail.com>
2025-04-29 10:57:54 -0300

    Add files via upload

--- desafio xadrez.c
+++ desafio xadrez.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+  int main(){
+
+  for (int i = 1; i <= 5; i++)
+  {
+    printf("mover torre para frente\n");
+  }
+
+  int iz = 1;
+  
+  while (iz <= 5)
+  {
+    printf("mover bispo na diagonal para direita %d\n", iz);
+    iz++;
+  }
+  
+  int i = 1;
+
+   do
+   {
+    printf("mover rainha para esquerda %d\n", i);
+    i++;
+   } while (i <= 8);
+   
+
+
+
+
+
+    return 0;
+  }
